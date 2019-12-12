906759c7e817f46cfc6ccb81dbb192ad55e35e7b
Anuja <step4@Step4s-MacBook-Pro.local>
2019-12-12 20:56:23 +0530

    changed variable names for points

--- src/line.js
+++ src/line.js
@@ -1,14 +1,16 @@
 class Line {
   constructor(x1, y1, x2, y2) {
-    (this.p1 = [x1, y1]), (this.p2 = [x2, y2]);
+    (this.a = [x1, y1]), (this.b = [x2, y2]);
   }
 
   toString() {
-    return `Line : (${this.p1.x},${this.p1.y}), (${this.p2.x},${this.p2.x})`;
+    return `Line : (${this.a.x},${this.a.y}), (${this.b.x},${this.b.x})`;
   }
 
-  isEqualTo(line) {
-    return this.p1.toString() == line.p1.toString() && this.p2.toString() == line.p2.toString();
+  isEqualTo(anotherLine) {
+    return (
+      this.a.toString() == anotherLine.a.toString() && this.b.toString() == anotherLine.b.toString()
+    );
   }
 }
 
