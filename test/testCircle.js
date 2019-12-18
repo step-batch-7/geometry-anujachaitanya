b5a06972b699e78f2c83c46d1603776b5c61980c
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2019-12-18 10:52:32 +0530

    modified moveTo function of circle and added tests accordingly

--- test/testCircle.js
+++ test/testCircle.js
@@ -89,7 +89,14 @@ describe("circle", () => {
     it("should create new circle for given parameters", () => {
       const firstCircle = new Circle({ x: 1, y: 4 }, 5);
       const movedCircle = new Circle({ x: 3, y: 4 }, 5);
-      assert.deepStrictEqual(firstCircle.moveTo({ x: 3, y: 4 }), movedCircle);
+      const point = new Point(3, 4);
+      assert.deepStrictEqual(firstCircle.moveTo(point), movedCircle);
+    });
+
+    it("should return null if given is not point", () => {
+      const firstCircle = new Circle({ x: 1, y: 4 }, 5);
+      const movedCircle = new Circle({ x: 3, y: 4 }, 5);
+      assert.isNull(firstCircle.moveTo({ x: 3, y: 4 }));
     });
   });
 
